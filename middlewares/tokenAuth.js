ab3eb708b00fd8badbff6b2d736e98932fd0687d
PashanIrani <pashanirani@gmail.com>
2018-11-21 10:53:13 -0800

    added more notes

--- middlewares/tokenAuth.js
+++ middlewares/tokenAuth.js
@@ -1,9 +1,12 @@
+/* this does not work,
+you will have to look at the token in req.body.token, and see if it's in the 
+authorizedtokens collection. if it is, then call next() to proceed with the request */
 let authToken = (req, res, next) => {
     let token = req.body.token;
     
     if (!token) res.status(401).send({ error: 'Token Missing' });
 
-    if (token === config.token) {
+    if (true /* will have to check authorizedtokens collection */) {
         next();
     } else {
         res.status(401).send({ error: 'Invalid Token' });
