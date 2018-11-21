27dd553c725ee2b6ad89528836a28eb6bd141573
Jake Choi <demikaiser13@gmail.com>
2018-11-20 22:01:07 -0800

    Database schema updated.

--- models/AuthorizedToken.js
+++ models/AuthorizedToken.js
@@ -1,19 +1,14 @@
 const mongoose = require('mongoose');
 
-let User = new mongoose.Schema({
-  user_id: String, // computed when user signs up
-  connected_external_apps: [app_ids],
-  credentials: {
-    email: String,
-    password: String // will be hashed
-  }
+let AuthorizedToken = new mongoose.Schema({
+  token: String
 });
 
 /**
  * Export functions for the outside use.
  */
 module.exports = {
-    User: mongoose.model('user', User),
+    AuthorizedToken: mongoose.model('authorizedToken', AuthorizedToken),
     connect: callback => {
         mongoose.connection
             .once('open', () => {
@@ -25,3 +20,5 @@ module.exports = {
             });
     }
 };
+
+
