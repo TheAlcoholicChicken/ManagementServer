a991a8a5c6ec9bd094f3e709fd28f7656560f27d
Justin Carey <justinthomascarey@gmail.com>
2018-11-22 08:33:02 -0800

    Refactored code

--- routes/user.js
+++ routes/user.js
@@ -13,19 +13,13 @@ module.exports = (app, db) => {
 
     /* Don't worry about hashing passwords for now. We will have to discuss methods for both python and node.js first irl */
     router.post('/create_account', authToken, (req, res) => {
-        // perfrom request
+        // perform request
         let user_email = req.body.user_email;
         let password = req.body.password;
         let token = req.body.token;
 
-        authTokenList.findOne({token}, (err, authToken) => {
-            if(authToken){
-                res.status(200).json({msg:"Success!"});
-                //next();
-            } else {
-                res.status(400).json({msg:"Token is not in DB."});
-            }
-        })
+        res.status(200).json({msg: "Account created."})
+
     });
  
     /* Don't worry about hashing passwords for now.*/
@@ -35,32 +29,18 @@ module.exports = (app, db) => {
         let password = req.body.password;
         let token = req.body.token;
 
-        authTokenList.findOne({token}, (err, authToken) => {
-            if(authToken){
-                res.status(200).json({msg:"Success!"});
-                //next();
-            } else {
-                res.status(400).json({msg:"Token is not in DB."});
-            }
-        })
+        res.status(200).json({msg: "Login successful."})
     });
 
     /* send the /user/get_badge_message post request to all external apps in 
     the DB (will be have to configiured first with the POST request in 'admin.js')
     then return a response like what's specifided in the docs */
     router.post('/get_badges', authToken, (req, res) => {
-        // perfrom request
+        // perform request
         let user_id = req.body.user_id;
         let token = req.body.token;
 
-        authTokenList.findOne({token}, (err, authToken) => {
-            if(authToken){
-                res.status(200).json({msg:"Success!"});
-                //next();
-            } else {
-                res.status(400).json({msg:"Token is not in DB."});
-            }
-        })
+        res.status(200).json({msg: "Badges retrieved."})
     });
     
     app.use('/user', router);
