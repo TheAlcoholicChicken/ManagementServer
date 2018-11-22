09ccc8d6fb93981b479de474eb552dddafe960d3
Justin Carey <justinthomascarey@gmail.com>
2018-11-21 21:26:31 -0800

    Token Validation for all three methods, no communication with the DB yet.

--- routes/user.js
+++ routes/user.js
@@ -8,15 +8,41 @@ for req and response structures.
 
 module.exports = (app, db) => {
     const authToken = require('../middlewares/tokenAuth')(db);
+    const authTokenList = require('../models/AuthorizedToken');
+    //const user_db = require('../models/User');
 
     /* Don't worry about hashing passwords for now. We will have to discuss methods for both python and node.js first irl */
     router.post('/create_account', authToken, (req, res) => {
         // perfrom request
-    });
+        let user_email = req.body.user_email;
+        let password = req.body.password;
+        let token = req.body.token;
 
+        authTokenList.findOne({token}, (err, authToken) => {
+            if(authToken){
+                res.status(200).json({msg:"Success!"});
+                //next();
+            } else {
+                res.status(400).json({msg:"Token is not in DB."});
+            }
+        })
+    });
+ 
     /* Don't worry about hashing passwords for now.*/
     router.post('/login', authToken, (req, res) => {
-        // perfrom request
+        // perform request
+        let user_email = req.body.user_email;
+        let password = req.body.password;
+        let token = req.body.token;
+
+        authTokenList.findOne({token}, (err, authToken) => {
+            if(authToken){
+                res.status(200).json({msg:"Success!"});
+                //next();
+            } else {
+                res.status(400).json({msg:"Token is not in DB."});
+            }
+        })
     });
 
     /* send the /user/get_badge_message post request to all external apps in 
@@ -24,6 +50,17 @@ module.exports = (app, db) => {
     then return a response like what's specifided in the docs */
     router.post('/get_badges', authToken, (req, res) => {
         // perfrom request
+        let user_id = req.body.user_id;
+        let token = req.body.token;
+
+        authTokenList.findOne({token}, (err, authToken) => {
+            if(authToken){
+                res.status(200).json({msg:"Success!"});
+                //next();
+            } else {
+                res.status(400).json({msg:"Token is not in DB."});
+            }
+        })
     });
     
     app.use('/user', router);
