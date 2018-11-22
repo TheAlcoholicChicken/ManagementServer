3cfea1de192282dec1afc16340971e6d896abeb3
PashanIrani <pashanirani@gmail.com>
2018-11-22 10:33:10 -0800

    so far

--- routes/user.js
+++ routes/user.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const errors = require('../controllers/errors')
 
 /*
 Look at https://thealcoholicchicken.github.io/Documentation/
@@ -9,27 +10,78 @@ for req and response structures.
 module.exports = (app, db) => {
     const authToken = require('../middlewares/tokenAuth')(db);
     const authTokenList = require('../models/AuthorizedToken');
+    const User = require('../models/User');
     //const user_db = require('../models/User');
 
     /* Don't worry about hashing passwords for now. We will have to discuss methods for both python and node.js first irl */
     router.post('/create_account', authToken, (req, res) => {
         // perform request
-        let user_email = req.body.user_email;
+        let email = req.body.user_email;
         let password = req.body.password;
-        let token = req.body.token;
 
-        res.status(200).json({msg: "Account created."})
+        User.findOne(
+            {  email, password  },
+            (err, user) => {
+                if (err) {
+                    errors.handle(err);
+                    res.status(400).json({ msg: err });
+                }
 
+                if (user == null) {
+                    User.create(
+                        {
+                            connected_external_apps: null,
+                            email,
+                            password // will need to be hashed
+                            
+                        },
+                        (err, User) => {
+                            if (err) {
+                                errors.handle(err);
+                                res.status(400).json({ msg: err });
+                            }
+
+                            if (User) {
+                                res.status(400).json({
+                                    msg: 'Great Success!'
+                                });
+                            }
+                        }
+                    );
+                } else {
+                    res.status(400).json({
+                        msg: 'User with ' + email + ' exists'
+                    });
+                }
+            }
+        );
     });
  
     /* Don't worry about hashing passwords for now.*/
     router.post('/login', authToken, (req, res) => {
         // perform request
-        let user_email = req.body.user_email;
+        let email = req.body.user_email;
         let password = req.body.password;
-        let token = req.body.token;
 
-        res.status(200).json({msg: "Login successful."})
+        User.findOne(
+            {  email, password  },
+            (err, user) => {
+                if (err) {
+                    errors.handle(err);
+                    res.status(400).json({ msg: err });
+                }
+
+                console.log(user);
+                if (user != null) {
+                    res.status(200).json({ msg: 'Login successful.' });
+                } else {
+                    res.status(400).json({
+                        msg: 'Login unsuccessful.'
+                    });
+                }
+            }
+        );
+
     });
 
     /* send the /user/get_badge_message post request to all external apps in 
@@ -37,10 +89,29 @@ module.exports = (app, db) => {
     then return a response like what's specifided in the docs */
     router.post('/get_badges', authToken, (req, res) => {
         // perform request
-        let user_id = req.body.user_id;
-        let token = req.body.token;
+        let email = req.body.user_email;
 
-        res.status(200).json({msg: "Badges retrieved."})
+        User.findOne(
+            {  email  },
+            (err, user) => {
+                if (err) {
+                    errors.handle(err);
+                    res.status(400).json({ msg: err });
+                }  
+                console.log(user);
+                
+                if (user != null) {
+                    console.log(user)
+                    res.status(200).json({
+                        msg: 'fgdhjk.'
+                    });
+                } else {
+                    res.status(400).json({
+                        msg: 'No user found.'
+                    });
+                }
+
+            });
     });
     
     app.use('/user', router);
